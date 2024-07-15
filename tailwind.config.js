745267699c088baf29a3da14557fe6beb0d34d08
Taajir <mgurey2023@mail.com>
2024-07-15 14:05:14 -0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      // fontFamily: {
+       
+      // },
+      colors: {
+        primary: "#ffffff",
+        secondary: "#ffffff",
+        brandDark: "#270c03",
+        dark: "#1e1e1e",
+        light: "#f5f5f5",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "3rem",
+        },
+      },
+      animation: {
+        "spin-slow": "spin 40s linear infinite",
+      },
+    },
+  },
+  plugins: [],
+};
